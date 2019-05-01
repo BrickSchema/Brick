3da29ccf6b3a793c798d0390aa8c0b58491136fd
Gabe Fierro <gtfierro225@gmail.com>
2019-04-30 18:14:03 -0700

    adding in more setpoints from original Brick definition

--- point.py
+++ point.py
@@ -15,6 +15,166 @@ point_definitions = {
     "Setpoint": {
         "tagvalues": [[BRICK.hasTag, TAG.Setpoint]],
         "subclasses": {
+            "Enthalpy_Setpoint": {},
+            "Dew_Point_Setpoint": {},
+            "Demand_Setpoint": {
+                "subclasses": {
+                    "Cooling_Demand_Setpoint": {},
+                    "Cooling_Request_Percent_Setpoint": {},
+                    "Cooling_Request_Setpoint": {},
+                    "Heating_Demand_Setpoint": {},
+                    "Heating_Request_Percent_Setpoint": {},
+                    "Heating_Request_Setpoint": {},
+                    "Preheat_Demand_Setpoint": {},
+                    "Discharge_Air_Flow_Demand_Setpoint": {},
+                    "Supply_Air_Flow_Demand_Setpoint": {},
+                },
+            },
+            "Damper_Position_Setpoint": {
+                "subclasses": {
+                    "Damper_Max_Position_Setpoint": {},
+                    "Damper_Min_Position_Setpoint": {},
+                },
+            },
+            "Dead_Band_Setpoint": {
+                "subclasses": {
+                    "Chilled_Water_Differential_Pressure_Dead_Band_Setpoint": {},
+                    "Cooling_Discharge_Air_Temperature_Dead_Band_Setpoint": {},
+                    "Cooling_Supply_Air_Temperature_Dead_Band_Setpoint": {},
+                    "Differential_Pressure_Dead_Band_Setpoint": {
+                        "subclasses": {
+                            "Chilled_Water_Pump_Differential_Pressure_Dead_Band_Setpoint": {},
+                            "Hot_Water_Differential_Pressure_Dead_Band_Setpoint": {},
+                        },
+                    },
+                    "Discharge_Air_Temperature_Dead_Band_Setpoint": {
+                        "subclasses": {
+                            "Heating_Discharge_Air_Temperature_Dead_Band_Setpoint": {},
+                            "Cooling_Discharge_Air_Temperature_Dead_Band_Setpoint": {},
+                        },
+                    },
+                    "Supply_Air_Temperature_Dead_Band_Setpoint": {
+                        "subclasses": {
+                            "Heating_Supply_Air_Temperature_Dead_Band_Setpoint": {},
+                            "Cooling_Supply_Air_Temperature_Dead_Band_Setpoint": {},
+                        },
+                    },
+                    "Temperature_Dead_Band_Setpoint": {
+                        "subclasses": {
+                            "Occupied_Cooling_Temperature_Dead_Band_Setpoint": {},
+                            "Occupied_Heating_Temperature_Dead_Band_Setpoint": {},
+                        },
+                    },
+                    "Air_Flow_Dead_Band_Setpoint": {
+                        "subclasses": {
+                            "Exhaust_Air_Stack_Flow_Dead_Band_Setpoint": {},
+                        },
+                    },
+                    "Static_Pressure_Dead_Band_Setpoint": {},
+                    "Supply_Water_Differential_Pressure_Dead_Band_Setpoint": {
+                        "subclasses": {
+                            "Thermal_Energy_Storage_Supply_Water_Differential_Pressure_Dead_Band_Setpoint": {},
+                        },
+                    },
+                    "Supply_Water_Temperature_Dead_Band_Setpoint": {
+                        "subclasses":{
+                            "Heat_Exchanger_Supply_Water_Temperature_Dead_Band_Setpoint": {}
+                        }
+                    },
+                },
+            },
+            "Flow_Setpoint": {
+                "tagvalues": [[BRICK.hasTag, TAG["Flow"]], [BRICK.hasTag, TAG["Setpoint"]]],
+                "subclasses": {
+                    "Air_Flow_Setpoint": {
+                        "tagvalues": [[BRICK.hasTag, TAG["Air"]], [BRICK.hasTag, TAG["Flow"]], [BRICK.hasTag, TAG["Setpoint"]]],
+                        "subclasses": {
+                            "Discharge_Air_Flow_Setpoint": {
+                                "subclasses": {
+                                    # Do the same for Max/Min? Occupied/Unoccupied?
+                                    "Discharge_Air_Flow_Demand_Setpoint": {},
+                                    "Cooling_Discharge_Air_Flow_Setpoint": {
+                                        "subclasses": {
+                                            "Cooling_Max_Discharge_Air_Flow_Setpoint": {
+                                                "subclasses": {
+                                                    "Occupied_Cooling_Max_Discharge_Air_Flow_Setpoint": {},
+                                                    "Unoccupied_Cooling_Max_Discharge_Air_Flow_Setpoint": {},
+                                                },
+                                            },
+                                            "Cooling_Min_Discharge_Air_Flow_Setpoint": {
+                                                "subclasses": {
+                                                    "Occupied_Cooling_Min_Discharge_Air_Flow_Setpoint": {},
+                                                    "Unoccupied_Cooling_Min_Discharge_Air_Flow_Setpoint": {},
+                                                },
+                                            },
+                                        },
+                                    },
+                                    "Heating_Discharge_Air_Flow_Setpoint": {
+                                        "subclasses": {
+                                            "Heating_Max_Discharge_Air_Flow_Setpoint": {
+                                                "subclasses": {
+                                                    "Occupied_Heating_Max_Discharge_Air_Flow_Setpoint": {},
+                                                    "Unoccupied_Heating_Max_Discharge_Air_Flow_Setpoint": {},
+                                                },
+                                            },
+                                            "Heating_Min_Discharge_Air_Flow_Setpoint": {
+                                                "subclasses": {
+                                                    "Occupied_Heating_Min_Discharge_Air_Flow_Setpoint": {},
+                                                    "Unoccupied_Heating_Min_Discharge_Air_Flow_Setpoint": {},
+                                                },
+                                            },
+                                        },
+                                    },
+                                },
+                            },
+                            "Exhaust_Air_Flow_Setpoint": {
+                                "subclasses": {
+                                    "Exhaust_Air_Stack_Flow_Setpoint": {},
+                                },
+                            },
+                            "Fan_Air_Flow_Setpoint": {},
+                            "Outside_Air_Flow_Setpoint": {
+                                "subclasses": {
+                                    "Min_Outside_Air_Flow_Setpoint": {},
+                                },
+                            },
+                            "Supply_Air_Flow_Setpoint": {
+                                "subclasses": {
+                                    "Supply_Air_Flow_Demand_Setpoint": {},
+                                    "Occupied_Supply_Air_Flow_Setpoint": {
+                                        "subclasses": {
+                                            "Occupied_Cooling_Supply_Air_Flow_Setpoint": {
+                                                "subclasses": {
+                                                    "Occupied_Cooling_Max_Supply_Air_Flow_Setpoint": {},
+                                                    "Occupied_Cooling_Min_Supply_Air_Flow_Setpoint": {},
+                                                },
+                                            },
+                                            "Occupied_Heating_Supply_Air_Flow_Setpoint": {
+                                                "subclasses": {
+                                                    "Occupied_Heating_Max_Supply_Air_Flow_Setpoint": {},
+                                                    "Occupied_Heating_Min_Supply_Air_Flow_Setpoint": {},
+                                                },
+                                            },
+                                        },
+                                    },
+                                    "Cooling_Supply_Air_Flow_Setpoint": {
+                                        "subclasses": {
+                                            "Cooling_Max_Supply_Air_Flow_Setpoint": {},
+                                            "Cooling_Min_Supply_Air_Flow_Setpoint": {},
+                                        },
+                                    },
+                                    "Heating_Supply_Air_Flow_Setpoint": {
+                                        "subclasses": {
+                                            "Heating_Max_Supply_Air_Flow_Setpoint": {},
+                                            "Heating_Min_Supply_Air_Flow_Setpoint": {},
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
             "Temperature_Setpoint": {
                 "tagvalues": [[BRICK.hasTag, TAG["Temperature"]], [BRICK.hasTag, TAG["Setpoint"]]],
                 "subclasses": {
