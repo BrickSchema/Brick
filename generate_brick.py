247d28b00d814ec57650f457cdca12d9c4ee59c5
Jason Koh <bk7749@gmail.com>
2019-08-14 23:20:17 -0700

    paramterize version in the code

--- generate_brick.py
+++ generate_brick.py
@@ -1,16 +1,15 @@
-from rdflib import Graph, Literal, BNode, Namespace, RDF, URIRef
+from rdflib import Graph, Literal, BNode, Namespace, RDF, URIRef, RDFS, OWL
 from rdflib.namespace import XSD
 from rdflib.collection import Collection
 from rdflib.extras.infixowl import Restriction
 import owlready2
 
 
-BRICK = Namespace("https://brickschema.org/schema/1.1.0/Brick#")
-TAG = Namespace("https://brickschema.org/schema/1.1.0/BrickTag#")
-BLDG = Namespace("https://brickschema.org/schema/1.1.0/ExampleBuilding#")
-OWL = Namespace("http://www.w3.org/2002/07/owl#")
-RDF = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
-RDFS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
+BRICK_VERSION = '1.1.0'
+
+BRICK = Namespace("https://brickschema.org/schema/{0}/Brick#".format(BRICK_VERSION))
+TAG = Namespace("https://brickschema.org/schema/{0}/BrickTag#".format(BRICK_VERSION))
+BLDG = Namespace("https://brickschema.org/schema/{0}/ExampleBuilding#".format(BRICK_VERSION))
 SKOS = Namespace("http://www.w3.org/2004/02/skos/core#")
 DCTERMS = Namespace("http://purl.org/dc/terms#")
 SDO = Namespace("http://schema.org#")
