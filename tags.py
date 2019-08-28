993dfe9a690b087864f357086060bcb8a4ab874b
Jason Koh <bk7749@gmail.com>
2019-08-28 14:46:33 -0700

    change Equip to Equipment

--- tags.py
+++ tags.py
@@ -12,11 +12,11 @@ tags = {
     "Heat": {},
     "Cool": {},
 
-    # all Equip have the Equip tag. This is accomplished by
+    # all Equipment have the Equip tag. This is accomplished by
     # using a Restriction onValue property that says that all members of
-    # the Equipment class have the "Equip" value for at least one of their tags.
-    # We could also accomplish this by having "rdfs:subclassof brick:Equipment" 
-    # be the same as "brick:hasTag bricktag:Equip".
+    # the Equipment class have the "Equipment" value for at least one of their tags.
+    # We could also accomplish this by having "rdfs:subclassof brick:Equipment"
+    # be the same as "brick:hasTag bricktag:Equipment".
     # We use a similar idiom for these other tags
     "Equipment": {},
     "Location": {},
