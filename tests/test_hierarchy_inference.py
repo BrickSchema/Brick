620b666e49d39927fb15b005c56dd73294ea903c
Gabe Fierro <gtfierro@cs.berkeley.edu>
2019-09-05 21:40:08 -0700

    fix time.get() -> time.time()

--- tests/test_hierarchy_inference.py
+++ tests/test_hierarchy_inference.py
@@ -85,7 +85,7 @@ def test_hierarchyinference():
             klass = row[0]
             entity = klass + entity_postfix  # Define an entity for the class
             g.add((entity, row[1], row[2]))  # Associate the entity with restrictions (i.e., Tags)
-        end_time = time.get()
+        end_time = time.time()
         print('Instantiation took {0} seconds'.format(int(end_time-start_time)))
 
         # Infer classes of the entities.
