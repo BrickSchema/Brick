2012e048aee6bb74364218d18f9663c7c0445dd0
Gabe Fierro <gtfierro@cs.berkeley.edu>
2019-09-13 14:12:31 -0700

    change G -> g for graph reference

--- tests/test_no_inference.py
+++ tests/test_no_inference.py
@@ -15,78 +15,78 @@ DCTERMS = Namespace("http://purl.org/dc/terms#")
 SDO = Namespace("http://schema.org#")
 A = RDF.type
 
-G = rdflib.Graph()
-G.parse('Brick.ttl', format='turtle')
-G.bind('rdf', RDF)
-G.bind('owl', OWL)
-G.bind('rdfs', RDFS)
-G.bind('skos', SKOS)
-G.bind('brick', BRICK)
-G.bind('tag', TAG)
-G.bind('bldg', BLDG)
+g = rdflib.Graph()
+g.parse('Brick.ttl', format='turtle')
+g.bind('rdf', RDF)
+g.bind('owl', OWL)
+g.bind('rdfs', RDFS)
+g.bind('skos', SKOS)
+g.bind('brick', BRICK)
+g.bind('tag', TAG)
+g.bind('bldg', BLDG)
 
 # Create instances
-G.add( (BLDG.Coil_1, A, BRICK.Heating_Coil) )
-G.add( (BLDG.AHU1, A, BRICK.Air_Handler_Unit) )
-G.add( (BLDG.VAV1, A, BRICK.Variable_Air_Volume_Box) )
-G.add( (BLDG.CH1, A, BRICK.Chiller) )
+g.add( (BLDG.Coil_1, A, BRICK.Heating_Coil) )
+g.add( (BLDG.AHU1, A, BRICK.Air_Handler_Unit) )
+g.add( (BLDG.VAV1, A, BRICK.Variable_Air_Volume_Box) )
+g.add( (BLDG.CH1, A, BRICK.Chiller) )
 
 # locations
-G.add( (BLDG.Zone1, A, BRICK.HVAC_Zone) )
-G.add( (BLDG.Room1, A, BRICK.Room) )
-G.add( (BLDG.Room2, A, BRICK.Room) )
+g.add( (BLDG.Zone1, A, BRICK.HVAC_Zone) )
+g.add( (BLDG.Room1, A, BRICK.Room) )
+g.add( (BLDG.Room2, A, BRICK.Room) )
 
 # points
-G.add( (BLDG.TS1, A, BRICK.Air_Temperature_Sensor) )
-G.add( (BLDG.TS2, A, BRICK.Air_Temperature_Sensor) )
-G.add( (BLDG.AFS1, A, BRICK.Air_Flow_Sensor) )
-G.add( (BLDG.co2s1, A, BRICK.CO2_Level_Sensor) )
+g.add( (BLDG.TS1, A, BRICK.Air_Temperature_Sensor) )
+g.add( (BLDG.TS2, A, BRICK.Air_Temperature_Sensor) )
+g.add( (BLDG.AFS1, A, BRICK.Air_Flow_Sensor) )
+g.add( (BLDG.co2s1, A, BRICK.CO2_Level_Sensor) )
 
-G.add( (BLDG.AFSP1, A, BRICK.Air_Flow_Setpoint) )
+g.add( (BLDG.AFSP1, A, BRICK.Air_Flow_Setpoint) )
 
-G.add( (BLDG.MAFS1, A, BRICK.Max_Air_Flow_Setpoint_Limit) )
+g.add( (BLDG.MAFS1, A, BRICK.Max_Air_Flow_Setpoint_Limit) )
 
 # establishing some relationships
-G.add( (BLDG.CH1, BRICK.feeds, BLDG.AHU1) )
-G.add( (BLDG.AHU1, BRICK.feeds, BLDG.VAV1) )
-G.add( (BLDG.VAV1, BRICK.hasPoint, BLDG.TS2) )
-G.add( (BLDG.VAV1, BRICK.hasPoint, BLDG.AFS1) )
-G.add( (BLDG.VAV1, BRICK.hasPoint, BLDG.AFSP1) )
+g.add( (BLDG.CH1, BRICK.feeds, BLDG.AHU1) )
+g.add( (BLDG.AHU1, BRICK.feeds, BLDG.VAV1) )
+g.add( (BLDG.VAV1, BRICK.hasPoint, BLDG.TS2) )
+g.add( (BLDG.VAV1, BRICK.hasPoint, BLDG.AFS1) )
+g.add( (BLDG.VAV1, BRICK.hasPoint, BLDG.AFSP1) )
 
-G.add( (BLDG.VAV1, BRICK.feeds, BLDG.Zone1) )
-G.add( (BLDG.Zone1, BRICK.hasPart, BLDG.Room1) )
-G.add( (BLDG.Zone1, BRICK.hasPart, BLDG.Room2) )
+g.add( (BLDG.VAV1, BRICK.feeds, BLDG.Zone1) )
+g.add( (BLDG.Zone1, BRICK.hasPart, BLDG.Room1) )
+g.add( (BLDG.Zone1, BRICK.hasPart, BLDG.Room2) )
 
-G.add( (BLDG.TS1, BRICK.hasLocation, BLDG.Room1) )
+g.add( (BLDG.TS1, BRICK.hasLocation, BLDG.Room1) )
 
 # lets us use both relationships
 from util.reasoner import reason_inverse_edges
-reason_inverse_edges(G)
+reason_inverse_edges(g)
 
 def make_readable(res):
     return [[uri.split('#')[-1] for uri in row] for row in res]
 
 def test_query_equipment():
-    res = make_readable(G.query("""SELECT DISTINCT ?equip WHERE {
+    res = make_readable(g.query("""SELECT DISTINCT ?equip WHERE {
     ?equip rdf:type/rdfs:subClassOf* brick:Equipment .
     }"""))
     assert len(res) ==  4
 
 def test_query_points():
-    res = make_readable(G.query("""SELECT DISTINCT ?point WHERE {
+    res = make_readable(g.query("""SELECT DISTINCT ?point WHERE {
     ?point rdf:type/rdfs:subClassOf* brick:Point .
     }"""))
     assert len(res) ==  6
 
 def test_query_sensors():
-    res = make_readable(G.query("""SELECT DISTINCT ?sensor WHERE {
+    res = make_readable(g.query("""SELECT DISTINCT ?sensor WHERE {
     ?sensor rdf:type/rdfs:subClassOf* brick:Sensor .
     }"""))
     assert len(res) ==  4
 
 def test_query_downstream_temperature():
     # temp sensors downstream of AHU1
-    res = make_readable(G.query("""SELECT DISTINCT ?thing ?point WHERE {
+    res = make_readable(g.query("""SELECT DISTINCT ?thing ?point WHERE {
     bldg:AHU1 (brick:feeds|brick:hasPart)* ?thing .
     ?thing (brick:hasPoint|brick:isLocationOf)  ?point .
     ?point rdf:type/rdfs:subClassOf* brick:Temperature_Sensor
@@ -95,7 +95,7 @@ def test_query_downstream_temperature():
 
 def test_query_room_temp_sensors_ahu1():
     # temp sensors downstream of AHU1
-    res = make_readable(G.query("""SELECT DISTINCT ?zone ?room ?sensor WHERE {
+    res = make_readable(g.query("""SELECT DISTINCT ?zone ?room ?sensor WHERE {
     bldg:AHU1 brick:feeds+ ?zone .
     ?zone brick:hasPart ?room .
     ?room rdf:type brick:Room .
