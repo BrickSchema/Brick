75cddae8853ad79722af59ea3b4c3c1e4818d946
Shreyas Nagare <shreyasnagare@gmail.com>
2019-10-14 14:37:53 -0400

    Add comments

--- update.py
+++ update.py
@@ -1,7 +1,15 @@
 from argparse import ArgumentParser
 from rdflib import Graph, Namespace
 from utils import find_conversions, convert, standardize_namespaces, bump_versions, backup
+from logging import getLogger
 
+
+"""
+Argument Parsing:
+source: source version (for example '1.0.3')
+target: target version (for example '1.1.0')
+info: log operation description
+"""
 parser = ArgumentParser(description='Update Brick models.')
 parser.add_argument('models', metavar='model', type=str, nargs='+',
                     help='a turtle file with a brick model')
@@ -9,24 +17,42 @@ parser.add_argument('--source',
                     help='source version', required=True)
 parser.add_argument('--target',
                     help='target version', required=True)
-
+parser.add_argument('--info',
+                    help='get information related to ongoing operations', action='store_true')
 args = parser.parse_args()
+
+
+# set log level to INFO if required.
+if args.info:
+    getLogger().setLevel("INFO")
+
+
+# Load the versions graph which has information about possible conversions.
 versions_graph = Graph()
 versions_graph.parse('./conversions/versions.ttl', format='turtle')
 versions_graph.bind('version', Namespace("https:brickschema.org/version#"))
+
+# Ask if the conversion is possible
 job = versions_graph.query("""ASK{
                     "%s" version:updatesTo+ "%s"
 }""" % (args.source, args.target))
 
-
+# If yes, find the shortest path and convert
 for doable in job:
     if doable:
         print("Conversion available!\n=====================")
+        # Find the conversions to update the model in minimum number of version upgrades
         conversions = find_conversions(args.source, args.target, versions_graph)
+
+        # Loop through all the input models
         for model in args.models:
             print('\n\nUpdating {}...'.format(model))
+
+            # Create a model.bak
             backup(model)
             standardize_namespaces(model)
+
+            # Execute all conversions
             for conversion in conversions:
                 model_graph = Graph()
                 model_graph.parse(model, format='turtle')
--- utils.py
+++ utils.py
@@ -6,12 +6,23 @@ from shutil import copyfile
 
 
 def find_conversions(source, target, versions_graph):
+    """
+    This finds the minimum number of version upgrades required to reach the target version.
+    :param source: source version
+    :param target: target version
+    :param versions_graph: graph to fetch update path from
+    :return: list of conversions in order of execution
+    """
     graph = Graph()
     for source_version, target_version in versions_graph.query("""SELECT ?source_version ?target_version{
                         ?source_version version:updatesTo ?target_version .
     }"""):
         graph.add_edge(str(source_version), str(target_version), {'conversions': 1})
+
+    # Find the shortest path
     res = find_path(graph, str(source), str(target), cost_func=lambda u, v, e, prev_e: e['conversions'])
+
+    # Create and return the conversions
     conversions = []
     print(' -> '.join(res.nodes))
     current = source
@@ -22,8 +33,17 @@ def find_conversions(source, target, versions_graph):
 
 
 def convert(conversion, model_graph):
+    """
+    This function coverts the model graph from one version to another
+    :param conversion: a tuple (from_version, to_version)
+    :param model_graph: the model to update
+    """
+
+    # Load conversion scripts
     with open('./conversions/{}-{}.json'.format(*conversion), 'r') as file:
         conversion_data = load(file)
+
+    # Add query namespaces
     namespaces = {}
     for prefix, namespace in conversion_data['namespaces'].items():
         namespaces[prefix] = Namespace(namespace)
@@ -33,6 +53,7 @@ def convert(conversion, model_graph):
 
 
 def standardize_namespaces(filename):
+    # This function standardises https on all brick namespaces
     with open(filename) as f:
         standardized_turtle = f.read().replace('http://brickschema.org',
                                    'https://brickschema.org')
@@ -41,6 +62,7 @@ def standardize_namespaces(filename):
 
 
 def bump_versions(filename, source, target):
+    # This function updates the version numbers of the model.
     with open(filename) as f:
         updated_turtle = f.read().replace('https://brickschema.org/schema/{}/Brick'.format(source),
                                    'https://brickschema.org/schema/{}/Brick'.format(target))
@@ -50,5 +72,6 @@ def bump_versions(filename, source, target):
 
 
 def backup(model):
+    # The models are backed up as $modelname.bak in the same directory
     backup_file = '{}.bak'.format(model)
     copyfile(model, backup_file)
