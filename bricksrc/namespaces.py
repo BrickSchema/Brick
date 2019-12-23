287b50654345cdc5542ae4edec8e9ddd91205d44
Gabe Fierro <gtfierro@cs.berkeley.edu>
2019-12-23 11:08:07 -0800

    Change Brick ontology versioning to be major.minor (ignore patch)
    Encoding the version of the ontology into the URI is important for
    indicating to users which versions of the ontology are "backwards
    compatible", but we do not want users to have to change their URIs
    often. I think that encoding the MAJOR and MINOR revision numbers into
    the URI (ignoring that PATCH revision) is a nice intermediate point.
    
    This commit encodes the MAJOR, MINOR and PATCH version numbers in
    `bricksrc/namespaces.py`, and exposes the MAJOR_MINOR and FULL_VERSION
    constants that provide the full semantic version.

--- bricksrc/namespaces.py
+++ bricksrc/namespaces.py
@@ -1,10 +1,14 @@
-from rdflib import Graph, Literal, BNode, Namespace, RDF, URIRef
+from rdflib import Namespace
 
-BRICK_VERSION = '1.1.0'
+BRICK_MAJOR_VERSION = 1
+BRICK_MINOR_VERSION = 1
+BRICK_PATCH_VERSION = 0
 
-BRICK = Namespace(f"https://brickschema.org/schema/{BRICK_VERSION}/Brick#")
-TAG = Namespace(f"https://brickschema.org/schema/{BRICK_VERSION}/BrickTag#")
-#BLDG = Namespace(f"https://brickschema.org/schema/{BRICK_VERSION}/ExampleBuilding#")
+MAJOR_MINOR = f"{BRICK_MAJOR_VERSION}.{BRICK_MINOR_VERSION}"
+FULL_VERSION = f"{MAJOR_MINOR}.{BRICK_PATCH_VERSION}"
+
+BRICK = Namespace(f"https://brickschema.org/schema/{MAJOR_MINOR}/Brick#")
+TAG = Namespace(f"https://brickschema.org/schema/{MAJOR_MINOR}/BrickTag#")
 OWL = Namespace("http://www.w3.org/2002/07/owl#")
 RDF = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
 RDFS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
@@ -13,6 +17,7 @@ DCTERMS = Namespace("http://purl.org/dc/terms#")
 SDO = Namespace("http://schema.org#")
 SOSA = Namespace("http://www.w3.org/ns/sosa#")
 A = RDF.type
+
 
 def bind_prefixes(g):
     g.bind('rdf', RDF)
