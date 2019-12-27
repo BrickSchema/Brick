1fc8768f6271138cf387aee3cc3219f5cb3aec7a
Gabe Fierro <gtfierro@cs.berkeley.edu>
2019-12-27 12:42:53 -0800

    Working on SHACL shapes to detect violations of disjoint classes
    One of the issues with validating the use of Brick properties is that
    the same RDFS inferencing that we use to determine the types of the
    entities in the graph also infers extra classes from the use of the
    properties. This means that we can't necessarily use a SHACL shape to
    make sure that the class of the object of a `hasLocation` relationship
    is actually a Location, because the use of the `hasLocation`
    relationship (and specifically, the `rdfs:range` property) infers the
    object of the property as a Location, even if it is already an instance
    of another root class like Equipment.
    
    What these new shapes do is generate a SHACL shape for each pair of root
    classes to detect any violations of the disjoint properties.
    
    There are also some small code style changes included in this commit

--- generate_shacl.py
+++ generate_shacl.py
@@ -1,16 +1,15 @@
-from rdflib import Graph, Literal, BNode, Namespace, RDF, URIRef, RDFS, OWL
-from rdflib.namespace import XSD
+from rdflib import Graph, Literal, BNode
 from rdflib.collection import Collection
-
-from bricksrc.namespaces import BRICK, RDF, OWL, DCTERMS, SDO, RDFS, SKOS, BRICK, TAG, SOSA, BSH, SH
+from bricksrc.namespaces import RDF, RDFS, BRICK, BSH, SH
 from bricksrc.namespaces import bind_prefixes
+# add SHACL shapes from properties
+from bricksrc.properties import properties as property_definitions
 
 G = Graph()
 bind_prefixes(G)
 A = RDF.type
-
-# add SHACL shapes from properties
-from bricksrc.properties import properties as property_definitions
+rootclasses = ["Equipment", "Location", "Point",
+               "Substance", "Quantity"]
 
 for name, properties in property_definitions.items():
 
@@ -21,33 +20,49 @@ for name, properties in property_definitions.items():
 
         if pred == RDFS.range:
             shapename = f"{name}RangeShape"
-            G.add( (BSH[shapename], SH['property'], sh_prop) )
-            G.add( (BSH[shapename], A, SH.NodeShape) )
+            G.add((BSH[shapename], SH['property'], sh_prop))
+            G.add((BSH[shapename], A, SH.NodeShape))
 
-            G.add( (sh_prop, SH['nodeKind'], SH.IRI) )
-            G.add( (sh_prop, SH['path'], BRICK[name]) )
-            G.add( (sh_prop, SH['class'], obj) )
-            qualifier = SH.targetObjectsOf
-            G.add( (BSH[shapename], qualifier, BRICK[name]) )
+            G.add((sh_prop, SH['nodeKind'], SH.IRI))
+            G.add((sh_prop, SH['path'], BRICK[name]))
+            G.add((sh_prop, SH['class'], obj))
+            G.add((BSH[shapename], SH.targetSubjectsOf, BRICK[name]))
+            G.add((sh_prop, SH['message'],
+                   Literal(f"Property {name} has object with incorrect type")))
 
-            G.add( (sh_prop, SH['message'], Literal(f"Property {name} has object with incorrect type")) )
 
         # TODO: broken SHACL for rdfs.domain
         #if pred == RDFS.domain:
         #    shapename = f"{name}DomainShape"
-        #    G.add( (BSH[shapename], SH['property'], sh_prop) )
-        #    G.add( (BSH[shapename], A, SH.NodeShape) )
+        #    G.add( (BSH[shapename], SH['property'], sh_prop))
+        #    G.add( (BSH[shapename], A, SH.NodeShape))
 
-        #    G.add( (sh_prop, SH['nodeKind'], SH.IRI) )
-        #    G.add( (sh_prop, SH['path'], BRICK[name]) )
+        #    G.add( (sh_prop, SH['nodeKind'], SH.IRI))
+        #    G.add( (sh_prop, SH['path'], BRICK[name]))
         #    qualifier = SH.targetClass
-        #    G.add( (BSH[shapename], qualifier, obj) )
+        #    G.add( (BSH[shapename], qualifier, obj))
 
-        #    G.add( (sh_prop, SH['message'], Literal(f"Property {name} has subject with incorrect type")) )
+        #    G.add( (sh_prop, SH['message'], Literal(f"Property {name} has subject with incorrect type")))
+
+# make sure that root classes are disjoint, even if inference has generated
+# some invalid properties (e.g. a subject of hasLocation will be inferred
+# as a Location even if it is already an Equipment).
+for rootclass1 in rootclasses:
+    for rootclass2 in rootclasses:
+        if rootclass1 == rootclass2:
+            continue
+        sh_prop = BNode()
+        shapename = f"{rootclass1}CanNotBe{rootclass2}Shape"
+        G.add((BSH[shapename], A, SH.NodeShape))
+        G.add((BSH[shapename], SH.targetClass, BRICK[rootclass1]))
+        G.add((BSH[shapename], SH["not"], sh_prop))
+        G.add((sh_prop, SH["class"], BRICK[rootclass2]))
+        G.add((sh_prop, SH["message"], Literal(f"Node cannot be both a \
+               {BRICK[rootclass1]} and a {BRICK[rootclass2]}. Check the \
+               properties it has")))
 
 # subproperties
 # TODO: explicitly coded for now, but we should generalize
-
 substance_subproperties = {
     'feedsAir':  {
         'substance': BRICK.Air,
@@ -65,9 +80,9 @@ for subprop, desc in substance_subproperties.items():
     for prop in desc['properties']:
         constraint = BNode()
         definition = BNode()
-        G.add( (constraint, SH['property'], definition) )
-        G.add( (definition, SH['path'], prop) )
-        G.add( (definition, SH['class'], desc['substance']) )
+        G.add( (constraint, SH['property'], definition))
+        G.add( (definition, SH['path'], prop))
+        G.add( (definition, SH['class'], desc['substance']))
         constraints.append(constraint)
 
     c = Collection(G, constraintlist, constraints)
