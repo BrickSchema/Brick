3e18ac1174fd361e8b11ba129c7c8d5ace914b31
Xiaolin (Charlene) Zang <czang@cmu.edu>
2020-01-09 12:55:12 -0500

    Use a link to Brick.ttl file in current dir for flexibility of using modified Brick.ttl.

--- shacl/m_verify.sh
+++ shacl/m_verify.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 python generate_shacl.py
-pyshacl -s shacl_test.ttl -i rdfs -a -m -f human -e ../Brick.ttl $1 2> metaErros
+pyshacl -s shacl_test.ttl -i rdfs -a -m -f human -e Brick.ttl $1 2> metaErros
