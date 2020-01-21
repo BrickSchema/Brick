ff2de8d2dbbbef5f96ea629adb89acb7d48875e5
Xiaolin (Charlene) Zang <czang@cmu.edu>
2020-01-21 12:35:49 -0500

    Use brick:expectedDomain and :expectedRange to replace RDFS.domain and .range for SHACL shape generateion.

--- bricksrc/properties.py
+++ bricksrc/properties.py
@@ -9,27 +9,40 @@ from .namespaces import *
 Defining properties
 """
 properties = {
+    # About expectedDomain and expectedRange: They replace temporarily
+    # RDFS.domin and RDFS.range respectively.  The .domain and .range
+    # properties confuse pre-inferencing of a SHACL validator without
+    # respecting disjointness.  Thus we want to remove them for now
+    # but still need the replacements to guide the shape generator.
+    "expectedDomain": {
+        A: [OWL.AsymmetricProperty, OWL.IrreflexiveProperty],
+        SKOS.definition: Literal("Temporary replacement for RDFS.domain"),
+    },
+    "expectedRange": {
+        A: [OWL.AsymmetricProperty, OWL.IrreflexiveProperty],
+        SKOS.definition: Literal("Temporary replacement for RDFS.range"),
+    },
     "isLocationOf": {
         A: [OWL.AsymmetricProperty, OWL.IrreflexiveProperty],
         OWL.inverseOf: "hasLocation",
-        SKOS.notation: [Literal("RDFS.domain: BRICK.Location")],
+        BRICK.expectedDomain: BRICK.Location,
         SKOS.definition: Literal("Subject is the physical location encapsulating the object"),
     },
     "hasLocation": {
         A: [OWL.AsymmetricProperty, OWL.IrreflexiveProperty],
         OWL.inverseOf: "isLocationOf",
-        SKOS.notation: [Literal("RDFS.range: BRICK.Location")],
+        BRICK.expectedRange: BRICK.Location,
         SKOS.definition: Literal("Subject is physically located in the location given by the object"),
     },
 
     "hasInputSubstance": {
         A: [OWL.AsymmetricProperty, OWL.IrreflexiveProperty],
-        SKOS.notation: [Literal("RDFS.range: BRICK.Substance")],
+        BRICK.expectedRange: BRICK.Substance,
         SKOS.definition: Literal("The subject receives the given substance as an input to its internal process"),
     },
     "hasOutputSubstance": {
         A: [OWL.AsymmetricProperty, OWL.IrreflexiveProperty],
-        SKOS.notation: [Literal("RDFS.range: BRICK.Substance")],
+        BRICK.expectedRange: BRICK.Substance,
         SKOS.definition: Literal("The subject produces or exports the given substance from its internal process"),
     },
 
@@ -75,12 +88,12 @@ properties = {
         A: [OWL.AsymmetricProperty, OWL.IrreflexiveProperty],
         OWL.inverseOf: "isPointOf",
         SKOS.definition: Literal("The subject has a digital/analog input/output point given by the object"),
-        SKOS.notation: [Literal("RDFS.range: BRICK.Point")],
+        BRICK.expectedRange: BRICK.Point,
     },
     "isPointOf": {
         A: [OWL.AsymmetricProperty, OWL.IrreflexiveProperty],
         OWL.inverseOf: "hasPoint",
-        SKOS.notation: [Literal("RDFS.domain: BRICK.Point")],
+        BRICK.expectedDomain: BRICK.Point,
     },
 
     "hasPart": {
@@ -97,35 +110,35 @@ properties = {
         A: [OWL.AsymmetricProperty, OWL.IrreflexiveProperty],
         OWL.inverseOf: "isTagOf",
         SKOS.definition: Literal("The subject has the given tag"),
-        SKOS.notation: [Literal("RDFS.range: BRICK.Tag")],
+        BRICK.expectedRange: BRICK.Tag,
     },
     "isTagOf": {
         A: [OWL.AsymmetricProperty, OWL.IrreflexiveProperty],
-        SKOS.notation: [Literal("RDFS.domain: BRICK.Tag")],
+        BRICK.expectedDomain: BRICK.Tag,
     },
 
     "measures": {
         A: [OWL.AsymmetricProperty, OWL.IrreflexiveProperty],
         OWL.inverseOf: "isMeasuredBy",
         SKOS.definition: Literal("The subject measures a quantity or substance given by the object"),
-        SKOS.notation: [Literal("RDFS.domain: BRICK.Point"),
-                        Literal("RDFS.range: BRICK.Measurable")],
+        BRICK.expectedDomain: BRICK.Point,
+        BRICK.expectedRange: BRICK.Measurable,
     },
     "isMeasuredBy": {
         A: [OWL.AsymmetricProperty, OWL.IrreflexiveProperty],
-        SKOS.notation: [Literal("RDFS.domain: BRICK.Measurable"),
-                        Literal("RDFS.range: BRICK.Point")],
+        BRICK.expectedDomain: BRICK.Measurable,
+        BRICK.expectedRange: BRICK.Point,
     },
     "regulates": {
         A: [OWL.AsymmetricProperty, OWL.IrreflexiveProperty],
         OWL.inverseOf: "isRegulatedBy",
         SKOS.definition: Literal("The subject contributes to or performs the regulation of the substance given by the object"),
-        SKOS.notation: [Literal("RDFS.domain: BRICK.Equipment"),
-                        Literal("RDFS.range: BRICK.Substance")],
+        BRICK.expectedDomain: BRICK.Equipment,
+        BRICK.expectedRange: BRICK.Substance,
     },
     "isRegulatedBy": {
         A: [OWL.AsymmetricProperty, OWL.IrreflexiveProperty],
-        SKOS.notation: [Literal("RDFS.domain: BRICK.Substance"),
-                        Literal("RDFS.range: BRICK.Equipment")],
+        BRICK.expectedDomain: BRICK.Substance,
+        BRICK.expectedRange: BRICK.Equipment,
     },
 }
--- shacl/generate_shacl.py
+++ shacl/generate_shacl.py
@@ -16,41 +16,30 @@ for name, properties in property_definitions.items():
 
     for pred, obj in properties.items():
         sh_prop = BNode()
-        if pred != SKOS.notation:
-            continue
 
-        # For now the shapes are generated with the guidance of RDFS.notation
-        # For example: (extracted from ../bricksrc/properties.py)
-        # SKOS.notation: [Literal("RDFS.domain: BRICK.Point"),
-        #                Literal("RDFS.range: BRICK.Measurable")],
-        # which dictates the subject and object types of the property.
+        # We are only concerned with properties that have RDFS.domain or .range
+        # predicate.  The temporary replacements for those predicates are
+        # brick:expectedDomain and :expectedRange.  See properties.py for
+        # explanation.
 
-        for elt in obj:
-            if str(elt).startswith('RDFS.range'):
-                (RDFS_range, range_elt) = str(elt).split()
-                range_value = eval(range_elt)
+        if pred == BRICK.expectedRange:
+            shapename = f"{name}RangeShape"
+            G.add((BSH[shapename], SH['property'], sh_prop))
+            G.add((BSH[shapename], A, SH.NodeShape))
 
-                shapename = f"{name}RangeShape"
-                G.add((BSH[shapename], SH['property'], sh_prop))
-                G.add((BSH[shapename], A, SH.NodeShape))
+            G.add((sh_prop, SH['path'], BRICK[name]))
+            G.add((sh_prop, SH['class'], obj))
+            G.add((BSH[shapename], SH.targetSubjectsOf, BRICK[name]))
+            G.add((sh_prop, SH['message'],
+                   Literal(f"Property {name} has object with incorrect type")))
 
-                G.add((sh_prop, SH['nodeKind'], SH.IRI))
-                G.add((sh_prop, SH['path'], BRICK[name]))
-                G.add((sh_prop, SH['class'], range_value))
-                G.add((BSH[shapename], SH.targetSubjectsOf, BRICK[name]))
-                G.add((sh_prop, SH['message'],
-                       Literal(f"Property {name} has object with incorrect type")))
-
-            if str(elt).startswith('RDFS.domain'):
-                (RDFS_domain, domain_elt) = str(elt).split()
-                domain_value = eval(domain_elt)
-
-                shapename = f"{name}DomainShape"
-                G.add( (BSH[shapename], A, SH.NodeShape))
-                G.add( (BSH[shapename], SH.targetSubjectsOf, BRICK[name]))
-                G.add( (BSH[shapename], SH['class'], domain_value))
-                G.add( (BSH[shapename], SH['message'],
-                        Literal(f"Property {name} has subject with incorrect type")))
+        if pred == BRICK.expectedDomain:
+            shapename = f"{name}DomainShape"
+            G.add( (BSH[shapename], A, SH.NodeShape))
+            G.add( (BSH[shapename], SH.targetSubjectsOf, BRICK[name]))
+            G.add( (BSH[shapename], SH['class'], obj))
+            G.add( (BSH[shapename], SH['message'],
+                    Literal(f"Property {name} has subject with incorrect type")))
 
 # subproperties
 # TODO: explicitly coded for now, but we should generalize
@@ -68,7 +57,6 @@ for subprop, desc in substance_subproperties.items():
         shapename = f"feedsAir{name.capitalize()}Shape"
         G.add((BSH[shapename], SH['property'], sh_prop))
         G.add((BSH[shapename], A, SH.NodeShape))
-        G.add((sh_prop, SH['nodeKind'], SH.IRI))
         G.add((sh_prop, SH['path'], prop))
         G.add((sh_prop, SH['class'], desc['substance']))
         G.add((sh_prop, SH['message'],
