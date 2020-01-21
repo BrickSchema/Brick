40c30cdbd080cdd13b49ceb48bf35200a82d9f7d
Xiaolin (Charlene) Zang <czang@cmu.edu>
2020-01-20 19:09:00 -0500

    Remove disjointness shapes. Add message to "feedsAir" shapes.

--- shacl/generate_shacl.py
+++ shacl/generate_shacl.py
@@ -11,10 +11,6 @@ from bricksrc.properties import properties as property_definitions
 G = Graph()
 bind_prefixes(G)
 A = RDF.type
-rootclasses = ["Equipment", "Location", "Point",
-               "Substance", "Quantity"]
-
-fluidClasses = ['Gas', 'Liquid']
 
 for name, properties in property_definitions.items():
 
@@ -56,39 +52,6 @@ for name, properties in property_definitions.items():
                 G.add( (BSH[shapename], SH['message'],
                         Literal(f"Property {name} has subject with incorrect type")))
 
-# make sure that root classes are disjoint, even if inference has generated
-# some invalid properties (e.g. a subject of hasLocation will be inferred
-# as a Location even if it is already an Equipment).
-'''
-for rootclass1 in rootclasses:
-    for rootclass2 in rootclasses:
-        if rootclass1 == rootclass2:
-            continue
-        sh_prop = BNode()
-        shapename = f"{rootclass1}CanNotBe{rootclass2}Shape"
-        G.add((BSH[shapename], A, SH.NodeShape))
-        G.add((BSH[shapename], SH.targetClass, BRICK[rootclass1]))
-        G.add((BSH[shapename], SH["not"], sh_prop))
-        G.add((sh_prop, SH["class"], BRICK[rootclass2]))
-        G.add((sh_prop, SH["message"], Literal(f"Node cannot be both a \
-               {BRICK[rootclass1]} and a {BRICK[rootclass2]}. Check the \
-               properties it has")))
-
-for rootclass1 in fluidClasses:
-    for rootclass2 in fluidClasses:
-        if rootclass1 == rootclass2:
-            continue
-        sh_prop = BNode()
-        shapename = f"{rootclass1}CanNotBe{rootclass2}Shape"
-        G.add((BSH[shapename], A, SH.NodeShape))
-        G.add((BSH[shapename], SH.targetClass, BRICK[rootclass1]))
-        G.add((BSH[shapename], SH["not"], sh_prop))
-        G.add((sh_prop, SH["class"], BRICK[rootclass2]))
-        G.add((sh_prop, SH["message"], Literal(f"Node cannot be both a \
-               {BRICK[rootclass1]} and a {BRICK[rootclass2]}. Check the \
-               properties it has")))
-'''
-
 # subproperties
 # TODO: explicitly coded for now, but we should generalize
 substance_subproperties = {
@@ -108,6 +71,9 @@ for subprop, desc in substance_subproperties.items():
         G.add((sh_prop, SH['nodeKind'], SH.IRI))
         G.add((sh_prop, SH['path'], prop))
         G.add((sh_prop, SH['class'], desc['substance']))
+        G.add((sh_prop, SH['message'],
+               Literal(f"Subject of property {subprop} has object with incorrect type")))
+
         G.add((BSH[shapename], SH.targetSubjectsOf, BRICK[subprop]))
 
 with open('shacl_test.ttl', 'wb') as f:
