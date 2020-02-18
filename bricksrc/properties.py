2059c45d4daa9b6883693c5aed656db24575dda1
Xiaolin (Charlene) Zang <czang@cmu.edu>
2020-02-18 13:22:22 -0500

    remove properties expectedDomain, expectedRange and hasSubproperty. Now properties.py is identical with the main repo's master.

--- bricksrc/properties.py
+++ bricksrc/properties.py
@@ -5,34 +5,6 @@ from .namespaces import A, OWL, RDFS, SKOS, BRICK
 Defining properties
 """
 properties = {
-    # About expectedDomain and expectedRange: They replace temporarily
-    # RDFS.domin and RDFS.range. The RDFS.domain and RDFS.range infer
-    # the type of the subject and object of a property, respectively.
-    # However, rather than imply the type of the subject or object by
-    # the use of the property, we want to verify that the property is
-    # used correctly given the types of the subject and object.
-    # Thus, we adopt the brick:expectedDomain and brick:expectedRange
-    # properties which imply nothing about the subject/object of the
-    # property they are annotating, but can be used to validate the type
-    # of their subject or object.  See shacl/DESIGN.md for more discussion.
-    "expectedDomain": {
-        A: [OWL.AsymmetricProperty, OWL.IrreflexiveProperty],
-        SKOS.definition: Literal("Temporary replacement for RDFS.domain"),
-    },
-
-    "expectedRange": {
-        A: [OWL.AsymmetricProperty, OWL.IrreflexiveProperty],
-        SKOS.definition: Literal("Temporary replacement for RDFS.range"),
-    },
-
-    "hasSubproperty": {
-        A: [OWL.AsymmetricProperty, OWL.IrreflexiveProperty],
-        OWL.inverseOf: RDFS.subPropertyOf,
-        RDFS.range: RDFS.Property,
-        RDFS.domain: RDFS.Property,
-        SKOS.definition: Literal("Subject is a property that has the object as its sub-properity")
-    },
-
     "isLocationOf": {
         A: [OWL.AsymmetricProperty, OWL.IrreflexiveProperty],
         OWL.inverseOf: BRICK["hasLocation"],
@@ -73,21 +45,20 @@ properties = {
         "subproperties": {
             "feedsAir": {
                 SKOS.definition: Literal("Passes air"),
+                # TODO: add restriction that it needs an air-based equipment on either side?
+                # this is equivalent with the classes that have :
+                # Restriction (onProperty=brick:hasInputSubstance, hasValue=brick:Air) AND
+                # Restriction (onProperty=brick:hasOutputSubstance, hasValue=brick:Air)
+
+                # looks something like this
+                #"domain_value_prop": [
+                #    [BRICK.hasTag, TAG.Air],
+                #],
+                #"range_value_prop": [
+                #    [BRICK.hasTag, TAG.Air],
+                #],
             },
         },
-
-        # TODO: add restriction that it needs an air-based equipment on either side?
-        # this is equivalent with the classes that have :
-        # Restriction (onProperty=brick:hasInputSubstance, hasValue=brick:Air) AND
-        # Restriction (onProperty=brick:hasOutputSubstance, hasValue=brick:Air)
-
-        # looks something like this
-        #"domain_value_prop": [
-        #    [BRICK.hasTag, TAG.Air],
-        #],
-        #"range_value_prop": [
-        #    [BRICK.hasTag, TAG.Air],
-        #],
     },
     "isFedBy": {
         A: [OWL.AsymmetricProperty, OWL.IrreflexiveProperty],
