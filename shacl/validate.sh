5620243ca7d2eec8bccf6c2e423886704423b5dc
Xiaolin (Charlen) Zang <charlene@xzang.net>
2020-02-25 17:19:29 -0500

    Delete files whose functions have been moved into brickschema repo. Add default BrickShape.ttl.

--- shacl/validate.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-echo "INFO: generate Brick.ttl in current directory..."
-python ../generate_brick.py || exit
-echo "INFO: generate shacl_test.ttl in current directory..."
-python generate_shacl.py || exit
-echo "INFO: execute python _pyshacl.py -s shacl_test.ttl -i rdfs -a -m -f human -e ModifiedBrick.ttl $1"
-python _pyshacl.py -s shacl_test.ttl -i rdfs -a -m -f human -e ModifiedBrick.ttl $1
