d8eaadb141ac92eab7a6f17df43fdf75c90b08a8
Jason Koh <bk7749@gmail.com>
2020-03-05 14:39:34 -0800

    move room under space

--- bricksrc/location.py
+++ bricksrc/location.py
@@ -15,54 +15,63 @@ location_subclasses = {
     "Outside": {"tags": [TAG.Outside, TAG.Location]},
     "City": {"tags": [TAG.City, TAG.Location]},
     "Wing": {"tags": [TAG.Wing, TAG.Location]},
-    "Space": {"tags": [TAG.Space, TAG.Location]},
+    "Space": {
+        "tags": [TAG.Space, TAG.Location],
+        "subclasses": {
+            "Room": {
+                "tags": [TAG.Room, TAG.Location],
+                "subclasses": {
+                    "Laboratory": {
+                        "tags": [TAG.Laboratory, TAG.Room, TAG.Location],
+                        "subclasses": {
+                            "Freezer": {
+                                "tags": [
+                                    TAG.Freezer,
+                                    TAG.Laboratory,
+                                    TAG.Room,
+                                    TAG.Location,
+                                ],
+                            },
+                            "Cold_Box": {
+                                "tags": [
+                                    TAG.Cold,
+                                    TAG.Box,
+                                    TAG.Laboratory,
+                                    TAG.Room,
+                                    TAG.Location,
+                                ],
+                            },
+                            "Hot_Box": {
+                                "tags": [
+                                    TAG.Hot,
+                                    TAG.Box,
+                                    TAG.Laboratory,
+                                    TAG.Room,
+                                    TAG.Location,
+                                ],
+                            },
+                            "Environment_Box": {
+                                "tags": [
+                                    TAG.Environment,
+                                    TAG.Box,
+                                    TAG.Laboratory,
+                                    TAG.Room,
+                                    TAG.Location,
+                                ],
+                            },
+                        },
+                    },
+                    "Server_Room": {"tags": [TAG.Server, TAG.Room, TAG.Location]},
+                },
+            },
+        },
+    },
     "Zone": {
         "tags": [TAG.Zone, TAG.Location],
         "subclasses": {
             "HVAC_Zone": {"tags": [TAG.HVAC, TAG.Zone, TAG.Location]},
             "Lighting_Zone": {"tags": [TAG.Lighting, TAG.Zone, TAG.Location]},
             "Fire_Zone": {"tags": [TAG.Fire, TAG.Zone, TAG.Location]},
-        },
-    },
-    "Room": {
-        "tags": [TAG.Room, TAG.Location],
-        "subclasses": {
-            "Laboratory": {
-                "tags": [TAG.Laboratory, TAG.Room, TAG.Location],
-                "subclasses": {
-                    "Freezer": {
-                        "tags": [TAG.Freezer, TAG.Laboratory, TAG.Room, TAG.Location],
-                    },
-                    "Cold_Box": {
-                        "tags": [
-                            TAG.Cold,
-                            TAG.Box,
-                            TAG.Laboratory,
-                            TAG.Room,
-                            TAG.Location,
-                        ],
-                    },
-                    "Hot_Box": {
-                        "tags": [
-                            TAG.Hot,
-                            TAG.Box,
-                            TAG.Laboratory,
-                            TAG.Room,
-                            TAG.Location,
-                        ],
-                    },
-                    "Environment_Box": {
-                        "tags": [
-                            TAG.Environment,
-                            TAG.Box,
-                            TAG.Laboratory,
-                            TAG.Room,
-                            TAG.Location,
-                        ],
-                    },
-                },
-            },
-            "Server_Room": {"tags": [TAG.Server, TAG.Room, TAG.Location]},
         },
     },
 }
