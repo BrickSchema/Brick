00b7f7072023db1dbfe8a3addff2616d5e02d552
Gabe Fierro <gtfierro@cs.berkeley.edu>
2020-04-17 12:52:15 -0700

    generate_brick handles lists of property values appropriately

--- generate_brick.py
+++ generate_brick.py
@@ -172,7 +172,11 @@ def define_classes(definitions, parent, pun_classes=False):
         ]
         for propname in other_properties:
             propval = defn[propname]
-            G.add((classname, propname, propval))
+            if isinstance(propval, list):
+                for pv in propval:
+                    G.add((classname, propname, pv))
+            else:
+                G.add((classname, propname, propval))
 
 
 def define_properties(definitions, superprop=None):
