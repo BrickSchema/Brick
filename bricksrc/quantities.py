3365daffdb487818b06b4ed4e69b1490e697702e
Gabe Fierro <gtfierro@cs.berkeley.edu>
2020-06-11 14:41:01 -0700

    remember to file peak power as a narrower concept of power

--- bricksrc/quantities.py
+++ bricksrc/quantities.py
@@ -151,6 +151,7 @@ quantity_definitions = {
                 },
             },
             "Peak_Power": {
+                SKOS.broader: QUDTQK.Power,
                 QUDT.applicableUnit: [UNIT.KiloW, UNIT.MegaW, UNIT.MilliW, UNIT.W],
                 QUDT.hasDimensionVector: QUDTDV["A0E0L2I0M1H0T-3D0"],
                 RDFS.isDefinedBy: URIRef(str(BRICK).strip("#")),
