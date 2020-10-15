c4ab3fdb92b2b54f56c7fbaeb20d3370e1830204
KiithNabaal <mike_czapik@yahoo.com>
2020-10-15 09:11:11 -0700

    Added intake air temp sensor

--- bricksrc/sensor.py
+++ bricksrc/sensor.py
@@ -1540,6 +1540,16 @@ sensor_definitions = {
                                             },
                                         },
                                     },
+                                    "Intake_Air_Temperature_Sensor": {
+                                        "tags": [
+                                            TAG.Point,
+                                            TAG.Outside,
+                                            TAG.Air,
+                                            TAG.Temperature,
+                                            TAG.Intake,
+                                            TAG.Sensor,
+                                        ],
+                                    },
                                 },
                             },
                         },
