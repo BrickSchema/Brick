9a54a324d51c537acfc37686de437c9fcee0a018
Gabe Fierro <gtfierro@cs.berkeley.edu>
2020-10-22 16:10:25 -0600

    switch to using shacl patterns

--- alignments/vbis/generate.py
+++ alignments/vbis/generate.py
@@ -1,11 +1,14 @@
 import csv
-from rdflib import Graph, Namespace, Literal
+from rdflib import Graph, Namespace, Literal, BNode
 from rdflib import RDF, RDFS, XSD, OWL
+from rdflib.collection import Collection
 
 graph = Graph()
 BRICK = Namespace("https://brickschema.org/schema/1.1/Brick#")
+SH = Namespace("http://www.w3.org/ns/shacl#")
 VBIS = Namespace("https://brickschema.org/schema/1.1/Brick/alignments/vbis/v3#")
 graph.bind("brick", BRICK)
+graph.bind("sh", SH)
 graph.bind("vbisalign", VBIS)
 
 # define mapping property
@@ -45,7 +48,24 @@ with open("vbis-brick.csv") as f:
         vbtags = get_vbis_tags(d)
         if len(vbtags) == 0:
             continue
-        for vb in vbtags:
-            graph.add((BRICK[bc], VBIS.hasVBISTag, Literal(vb)))
+        # create a shape for each set of tags
+        graph.add((BRICK[bc], RDF.type, SH.NodeShape))
+        patterns = []
+        if len(vbtags) == 1:
+            shape = BNode()
+            graph.add((BRICK[bc], SH.property, shape))
+            graph.add((shape, RDF.type, SH.PropertyShape))
+            graph.add((shape, SH.path, VBIS.hasVBISTag))
+            graph.add((shape, SH.pattern, Literal(vbtags[0])))
+        else:
+            shapeList = BNode()
+            graph.add((BRICK[bc], SH["or"], shapeList))
+            for vb in vbtags:
+                pattern = BNode()
+                patterns.append(pattern)
+                graph.add((pattern, RDF.type, SH.PropertyShape))
+                graph.add((pattern, SH.path, VBIS.hasVBISTag))
+                graph.add((pattern, SH.pattern, Literal(vb)))
+            Collection(graph, shapeList, patterns)
 
 graph.serialize("Brick-VBIS-alignment.ttl", format="turtle")
