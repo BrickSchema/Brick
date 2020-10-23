4ea5b634987fe81fded5db3a6ca8a363743454a1
Gabe Fierro <gtfierro@cs.berkeley.edu>
2020-10-23 10:20:17 -0600

    fix unnecessary f-string

--- bricksrc/namespaces.py
+++ bricksrc/namespaces.py
@@ -4,7 +4,7 @@ from .version import BRICK_VERSION
 BRICK = Namespace(f"https://brickschema.org/schema/{BRICK_VERSION}/Brick#")
 TAG = Namespace(f"https://brickschema.org/schema/{BRICK_VERSION}/BrickTag#")
 BSH = Namespace(f"https://brickschema.org/schema/{BRICK_VERSION}/BrickShape#")
-SH = Namespace(f"http://www.w3.org/ns/shacl#")
+SH = Namespace("http://www.w3.org/ns/shacl#")
 OWL = Namespace("http://www.w3.org/2002/07/owl#")
 RDF = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
 RDFS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
