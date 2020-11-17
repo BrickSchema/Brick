d4c542425cb4d9a394566c81bd9974034c12e4b5
Gabe Fierro <gtfierro@cs.berkeley.edu>
2020-11-17 11:35:53 -0800

    fixing generation

--- alignments/vbis/generate.py
+++ alignments/vbis/generate.py
@@ -1,4 +1,5 @@
 import csv
+import re
 from rdflib import Graph, Namespace, Literal, BNode
 from rdflib import RDF, RDFS, XSD, OWL
 from rdflib.collection import Collection
@@ -26,10 +27,23 @@ def get_brick_class(d):
 
 def rewrite_vbis_pattern(pat):
     """
+    If '*' is in the pattern or there are fewer than 3 '-' in the pattern, then
+    we return a rewritten regex; else we return the pattern (which should be a
+    fully-qualified VBIS tag).  The first returned value is True if the value
+    is a pattern, and False otherwise.
+
     Rewrite VBIS patterns to match the format of regular expressions
     required by XML schema
     """
-    return "^" + pat.strip().replace("*", ".*") + "$"
+    if "*" in pat:
+        newpat = "^" + re.sub(r"-?\*", "-.*", pat.strip()) + "$"
+        return True, newpat
+    elif len(re.findall("-", pat)) < 3:
+        # treat this as a prefix
+        if pat.endswith("-"):
+            pat = pat.rstrip("-")
+        return True, "^" + pat.strip() + "-.*$"
+    return False, pat
 
 
 def get_vbis_tags(d):
@@ -45,36 +59,71 @@ def get_vbis_tags(d):
     return vbis_tags
 
 
+# use a set to eliminate duplicate rows in the CSV file
+finished_brick_classes = set()
 with open("vbis-brick-v4.csv") as f:
     r = csv.reader(f)
     header = next(r)
     for row in r:
         d = dict(zip(header, row))
         bc = get_brick_class(d)
-        if bc is None:
+        if bc is None or bc in finished_brick_classes:
             continue
-        vbtags = get_vbis_tags(d)
-        if len(vbtags) == 0:
+        finished_brick_classes.add(bc)
+        tagsOrPatterns = get_vbis_tags(d)
+        if len(tagsOrPatterns) == 0:
             continue
+        print(f"Defining shapes for {bc}")
         # create a shape for each set of tags
         graph.add((VBIS[f"{bc}Shape"], RDF.type, SH.NodeShape))
         graph.add((VBIS[f"{bc}Shape"], SH.targetClass, BRICK[bc]))
+
+        # patterns is the list of patterns that match this Brick class
         patterns = []
-        if len(vbtags) == 1:
+        # vbtags is the list of fully qualified VB tags that match this Brick class
+        vbtags = []
+        # loop through all of the tags and/or patterns we get and sort them into
+        # the above two lsits
+        for vb in tagsOrPatterns:
+            isPattern, patOrValue = rewrite_vbis_pattern(vb)
+            if isPattern:
+                patterns.append(patOrValue)
+            else:
+                vbtags.append(patOrValue)
+        print(f"{len(patterns)} patterns; {len(vbtags)} full tags")
+
+        if len(patterns) > 0:
+            # handle patterns; this is either a SH:pattern directly (if there is one)
+            # or a SH:or of a set of patterns (if there is > 1)
+            if len(patterns) == 1:
+                shape = BNode()
+                graph.add((VBIS[f"{bc}Shape"], SH.property, shape))
+                graph.add((shape, RDF.type, SH.PropertyShape))
+                graph.add((shape, SH.path, VBIS.hasVBISTag))
+                graph.add((shape, SH.pattern, Literal(patterns[0])))
+            else:
+                shapeList = BNode()
+                graph.add((VBIS[f"{bc}Shape"], SH["or"], shapeList))
+                patternList = []
+                for vbtag in patterns:
+                    pattern = BNode()
+                    patternList.append(pattern)
+                    graph.add((pattern, RDF.type, SH.PropertyShape))
+                    graph.add((pattern, SH.path, VBIS.hasVBISTag))
+                    graph.add((pattern, SH.pattern, Literal(vbtag)))
+                Collection(graph, shapeList, patternList)
+        elif len(vbtags) > 0:
+            # handle fully qualified tags. If there is one, then it is a mandatory
+            # value. If there are multiple, then we must have one of them as a value
             shape = BNode()
             graph.add((VBIS[f"{bc}Shape"], SH.property, shape))
             graph.add((shape, RDF.type, SH.PropertyShape))
             graph.add((shape, SH.path, VBIS.hasVBISTag))
-            graph.add((shape, SH.pattern, Literal(rewrite_vbis_pattern(vbtags[0]))))
-        else:
-            shapeList = BNode()
-            graph.add((VBIS[f"{bc}Shape"], SH["or"], shapeList))
-            for vb in vbtags:
-                pattern = BNode()
-                patterns.append(pattern)
-                graph.add((pattern, RDF.type, SH.PropertyShape))
-                graph.add((pattern, SH.path, VBIS.hasVBISTag))
-                graph.add((pattern, SH.pattern, Literal(rewrite_vbis_pattern(vb))))
-            Collection(graph, shapeList, patterns)
+            if len(vbtags) == 1:
+                graph.add((shape, SH.hasValue, Literal(vbtags[0])))
+            else:
+                valueList = BNode()
+                graph.add((shape, SH["in"], valueList))
+                Collection(graph, valueList, [Literal(x) for x in vbtags])
 
 graph.serialize("Brick-VBIS-alignment.ttl", format="turtle")
