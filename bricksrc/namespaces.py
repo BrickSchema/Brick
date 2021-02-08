0cc6dde16addf9c692f65ced75e75ec735b27732
Gabe Fierro <gtfierro@cs.berkeley.edu>
2021-02-08 08:56:57 -0700

    changing Brick namespace to be version-independent

--- bricksrc/namespaces.py
+++ bricksrc/namespaces.py
@@ -1,9 +1,9 @@
 from rdflib import Namespace
 from .version import BRICK_VERSION
 
-BRICK = Namespace(f"https://brickschema.org/schema/{BRICK_VERSION}/Brick#")
-TAG = Namespace(f"https://brickschema.org/schema/{BRICK_VERSION}/BrickTag#")
-BSH = Namespace(f"https://brickschema.org/schema/{BRICK_VERSION}/BrickShape#")
+BRICK = Namespace("https://brickschema.org/schema/Brick#")
+TAG = Namespace("https://brickschema.org/schema/BrickTag#")
+BSH = Namespace("https://brickschema.org/schema/BrickShape#")
 SH = Namespace("http://www.w3.org/ns/shacl#")
 XSD = Namespace("http://www.w3.org/2001/XMLSchema#")
 OWL = Namespace("http://www.w3.org/2002/07/owl#")
