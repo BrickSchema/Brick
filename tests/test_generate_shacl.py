ccc7a437a1c1ed3cce7d1a7e9b47874fe9ce6c25
Jason Koh <bk7749@gmail.com>
2021-03-25 22:51:21 -0700

    remove old logic for shacl test

--- tests/test_generate_shacl.py
+++ tests/test_generate_shacl.py
@@ -9,31 +9,3 @@ from bricksrc.properties import properties  # noqa: E402
 g = brickschema.Graph()
 g.load_file("shacl/BrickShape.ttl")
 bind_prefixes(g)
-
-
-def test_domainProperties():
-    for (name, props) in properties.items():
-        if RDFS.domain in props:
-            q = f"""SELECT ?shape WHERE {{
-            ?shape a sh:NodeShape .
-            ?shape sh:targetSubjectsOf brick:{name} .
-            ?shape sh:class <{props[RDFS.domain]}> . }}
-            """
-            res = make_readable(g.query(q))
-            assert len(res) == 1, "unexpected # of query results"
-    return
-
-
-def test_rangeProperties():
-    for (name, props) in properties.items():
-        if RDFS.range in props:
-            q = f"""SELECT ?shape WHERE {{
-            ?shape a sh:NodeShape .
-            ?shape sh:property [
-            sh:class <{props[RDFS.range]}> ;
-            sh:path brick:{name} ;
-            ] }}
-            """
-            res = make_readable(g.query(q))
-            assert len(res) == 1, "unexpected # of query results"
-    return
