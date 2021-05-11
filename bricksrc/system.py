0556f03e588e1f7953f295abb627dfd09f4d6a45
Carlos Duarte <cduarte@berkeley.edu>
2021-05-11 14:51:45 -0700

    added air system classes

--- bricksrc/system.py
+++ bricksrc/system.py
@@ -13,6 +13,21 @@ system_subclasses = {
         "tags": [TAG.Heat, TAG.Ventilation, TAG.Air, TAG.Conditioning, TAG.System],
         "subclasses": {
             "Steam_System": {"tags": [TAG.Steam, TAG.System]},
+            "Air_System": {
+                "tags": [
+                    TAG.Air,
+                    TAG.System,
+                ],
+                "subclasses": {
+                    "Ventilation_Air_System": {
+                        "tags": [
+                            TAG.Ventilation,
+                            TAG.Air,
+                            TAG.System,
+                        ],
+                    },
+                },
+            },
             "Water_System": {
                 "tags": [TAG.Water, TAG.System],
                 "subclasses": {
