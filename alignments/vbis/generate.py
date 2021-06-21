f554b4e62a0629899b9f287bc1021e07b2b9f07d
Gabe Fierro <gtfierro@cs.berkeley.edu>
2021-06-21 14:55:07 -0700

    fix generation

--- alignments/vbis/generate.py
+++ alignments/vbis/generate.py
@@ -8,6 +8,7 @@ from brickschema.namespaces import BRICK, SH, A, RDFS, SKOS
 
 
 g = brickschema.Graph().load_file("base.ttl")
+brick = brickschema.Graph().load_file("../../Brick.ttl")
 
 VBIS = Namespace("https://brickschema.org/schema/Brick/alignments/vbis#")
 g.bind("vbis", VBIS)
@@ -20,6 +21,11 @@ vbistags = set()
 def num_sections(tag):
     """Returns the number of VBIS sections in the tag"""
     return tag.count("-") + 1
+
+
+def get_subclasses(brickclass):
+    res = brick.query(f"SELECT ?sc WHERE {{ ?sc rdfs:subClassOf+ <{brickclass}> }}")
+    return [row[0] for row in res]
 
 
 with open("vbis-brick.csv") as f:
@@ -44,12 +50,18 @@ for tagA in vbistags:
         elif tagB.startswith(tagA) and num_sections(tagB) == num_sections(tagA) + 1:
             g.add((VBIS[tagA], SKOS.narrower, VBIS[tagB]))
 
+# use Brick class structure to 'inherit' tags from subclasses
+for brickclass in mapping:
+    for subclass in get_subclasses(BRICK[brickclass]):
+        subclass_tags = mapping.get(subclass.split("#")[-1], [])
+        mapping[brickclass].extend(subclass_tags)
+
 # generate SHACL shapes for Brick/VBIS alignment
 for brickclass, vbistags in mapping.items():
     shape = f"{brickclass}TagShape"
 
     taglist = BNode()
-    vbistags = [VBIS[tag] for tag in vbistags]
+    vbistags = [VBIS[tag] for tag in set(vbistags)]
     Collection(g, taglist, vbistags)
 
     g.add((VBIS[shape], A, SH.NodeShape))
@@ -59,6 +71,12 @@ for brickclass, vbistags in mapping.items():
             VBIS[shape],
             SH.property,
             [
+                (
+                    SH.message,
+                    Literal(
+                        f"Brick class brick:{brickclass} does not match the provided tag"
+                    ),
+                ),
                 (SH["path"], VBIS.hasTag),
                 (SH["in"], taglist),
                 (SH["maxCount"], Literal(1)),
