cfdded830417160e9c9d7d577357e787f4f3727b
Gabe Fierro <gtfierro@cs.berkeley.edu>
2021-07-29 14:50:49 -0600

    Adding Differential Quantities to Brick (#294)
    * try differential static pressure
    
    * working on more differential quantities, adding annotations to sensors
    
    * fix annotation
    
    * fix some quantities
    
    * fix inference related to differential sensors
    
    * fix co2 sensor query test

--- bricksrc/quantities.py
+++ bricksrc/quantities.py
@@ -45,6 +45,17 @@ quantity_definitions = {
                 RDFS.isDefinedBy: URIRef(str(BRICK).strip("#")),
                 RDFS.label: Literal("COConcentration"),
                 SKOS.broader: QUDTQK.DimensionlessRatio,
+                SKOS.narrower: {
+                    "Differential_CO_Concentration": {
+                        QUDT.isDeltaQuantity: Literal(True),
+                        QUDT.applicableUnit: [UNIT.PPM, UNIT.PPB],
+                        SKOS.definition: Literal(
+                            "The difference in carbon monoxide concentration between two areas"
+                        ),
+                        RDFS.isDefinedBy: URIRef(str(BRICK).strip("#")),
+                        RDFS.label: Literal("ΔCOConcentration"),
+                    },
+                },
             },
             "CO2_Concentration": {
                 QUDT.applicableUnit: [UNIT.PPM, UNIT.PPB],
@@ -55,6 +66,17 @@ quantity_definitions = {
                 RDFS.isDefinedBy: URIRef(str(BRICK).strip("#")),
                 RDFS.label: Literal("CO2Concentration"),
                 SKOS.broader: QUDTQK.DimensionlessRatio,
+                SKOS.narrower: {
+                    "Differential_CO2_Concentration": {
+                        QUDT.isDeltaQuantity: Literal(True),
+                        QUDT.applicableUnit: [UNIT.PPM, UNIT.PPB],
+                        SKOS.definition: Literal(
+                            "The difference in carbon dioxide concentration between two areas"
+                        ),
+                        RDFS.isDefinedBy: URIRef(str(BRICK).strip("#")),
+                        RDFS.label: Literal("ΔCO2Concentration"),
+                    },
+                },
             },
             "Formaldehyde_Concentration": {
                 QUDT.applicableUnit: [UNIT.PPM, UNIT.PPB],
@@ -557,7 +579,15 @@ quantity_definitions = {
                 BRICK.hasQUDTReference: QUDTQK["AtmosphericPressure"]
             },
             "Dynamic_Pressure": {},
-            "Static_Pressure": {BRICK.hasQUDTReference: QUDTQK["StaticPressure"]},
+            "Static_Pressure": {
+                BRICK.hasQUDTReference: QUDTQK["StaticPressure"],
+                SKOS.narrower: {
+                    "Differential_Static_Pressure": {
+                        BRICK.hasQUDTReference: QUDTQK["StaticPressure"],
+                        QUDT.isDeltaQuantity: Literal(True),
+                    },
+                },
+            },
             "Velocity_Pressure": {
                 BRICK.hasQUDTReference: [
                     QUDTQK["DynamicPressure"],
@@ -608,6 +638,10 @@ quantity_definitions = {
     "Temperature": {
         BRICK.hasQUDTReference: QUDTQK["ThermodynamicTemperature"],
         SKOS.narrower: {
+            "Differential_Temperature": {
+                BRICK.hasQUDTReference: QUDTQK["ThermodynamicTemperature"],
+                QUDT.isDeltaQuantity: Literal(True),
+            },
             "Operative_Temperature": {
                 QUDT.applicableUnit: [UNIT["DEG_F"], UNIT["DEG_C"], UNIT["K"]],
                 SKOS.definition: Literal(
@@ -637,6 +671,12 @@ quantity_definitions = {
                 RDFS.isDefinedBy: URIRef(str(BRICK).strip("#")),
                 RDFS.label: Literal("Dry_Bulb_Temperature"),
                 SKOS.broader: QUDTQK.ThermodynamicTemperature,
+                SKOS.narrower: {
+                    "Differential_Dry_Bulb_Temperature": {
+                        BRICK.hasQUDTReference: QUDTQK["Dry_Bulb_Temperature"],
+                        QUDT.isDeltaQuantity: Literal(True),
+                    },
+                },
             },
             "Wet_Bulb_Temperature": {
                 QUDT.applicableUnit: [UNIT["DEG_F"], UNIT["DEG_C"], UNIT["K"]],
