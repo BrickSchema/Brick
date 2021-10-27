871dfee1444d0e9ff102987ca436fd3798b96dfc
Gabe Fierro <gtfierro@cs.berkeley.edu>
2021-10-27 11:18:14 -0600

    fix up RDF

--- bricksrc/entity_properties.py
+++ bricksrc/entity_properties.py
@@ -388,7 +388,7 @@ digital_representation_shapes = {
             BRICK["read-property"]: {
                 "datatype": XSD.string,
                 "optional": True,
-                SH["defaultValue"]: "present-value",
+                SH["defaultValue"]: Literal("present-value"),
             },
         },
     },
