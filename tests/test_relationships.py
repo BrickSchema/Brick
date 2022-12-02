2db1912912c2edb7e5144636e0bd2f9bacd3e31a
Gabe Fierro <gtfierro@mines.edu>
2022-12-01 23:18:15 -0700

    fix test

--- tests/test_relationships.py
+++ tests/test_relationships.py
@@ -3,12 +3,12 @@ from brickschema.namespaces import BRICK, RDFS
 import sys
 
 sys.path.append("..")
-from bricksrc.properties import properties  # noqa:E402
+from bricksrc.relationships import relationships  # noqa:E402
 
 
 def test_relationship_identification():
     g = brickschema.Graph()
     g.load_file("Brick.ttl")
 
-    props = list(g.subjects(RDFS.subPropertyOf, BRICK.Relationship))
-    assert len(props) == len(properties)
+    relships = list(g.subjects(RDFS.subPropertyOf, BRICK.Relationship))
+    assert len(relships) == len(relationships)
