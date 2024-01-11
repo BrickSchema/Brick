3440f1f7a6222ec9122f29053e7d7d38848d8159
Gabe Fierro <gtfierro@mines.edu>
2024-01-11 09:48:10 -0700

    Add EVSE Charging Concepts (#529)
    * ensure that deprecated concepts are not always asserted to be owl:Class
    
    * adding some evse concepts
    
    * add evse example
    
    * add directionality and expand enumerations
    
    * working on more EVSe rules; put directionality on the port
    
    * implement inference rules for charging directionality
    
    * add test for EVSE example
    
    * fix EVSE property definitions
    
    * add inverse property rule
    
    * fix datatypes on evse example
    
    * use topquadrant reasoning on test
    
    * use specialized logger
    
    * final changes

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -10,8 +10,27 @@ def test_example_file_with_reasoning(brick_with_imports, filename):
     g = brick_with_imports
     g.load_file(filename)
     env.import_dependencies(g)
-    # g.expand("shacl", backend="topquadrant")
-    # g.serialize("/tmp/res.ttl")
+    g.expand("shacl", backend="topquadrant")
 
     valid, _, report = g.validate(engine="topquadrant")
     assert valid, report
+
+
+# specific unit test for the 'evse.ttl' example file
+def test_evse_example_file_with_reasoning(brick_with_imports):
+    g = brick_with_imports
+    g.load_file("examples/evse/evse.ttl")
+    env.import_dependencies(g)
+    g.expand("shacl", backend="topquadrant")
+
+    # test that all Electric_Vehicle_Charging_Ports in the model
+    # have a brick:electricVehicleChargerDirectionality property
+    q = """
+    SELECT ?evcp WHERE {
+        ?evcp a/rdfs:subClassOf* brick:Electric_Vehicle_Charging_Port .
+        FILTER NOT EXISTS {
+            ?evcp brick:electricVehicleChargerDirectionality ?dir .
+        }
+    }"""
+    res = list(g.query(q))
+    assert len(res) == 0, "All EVCPs must have a directionality property"
