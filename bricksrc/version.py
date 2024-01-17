9bcc33f9c8fd9fa81bc7ed4252a33718e2dec7d9
Gabe Fierro <gtfierro@mines.edu>
2024-01-16 22:54:40 -0700

    bump dependency and version

--- bricksrc/version.py
+++ bricksrc/version.py
@@ -3,7 +3,7 @@ BRICK_MAJOR_VERSION = 1
 
 # Minor version: increment for substantial additions to the ontology that
 # are backwards compatible
-BRICK_MINOR_VERSION = 3
+BRICK_MINOR_VERSION = 4
 
 # Patch version: increment for minor additions/changes to the ontology that
 # are largely backwards compatible (bug-fixes exempted)
@@ -11,7 +11,7 @@ BRICK_PATCH_VERSION = 0
 
 # the simplified (no patch version) version number for Brick. Intended for
 # inclusion in the Brick namespace URI
-BRICK_VERSION = f"{BRICK_MAJOR_VERSION}.{BRICK_MINOR_VERSION}"
+BRICK_VERSION = f"{BRICK_MAJOR_VERSION}.{BRICK_MINOR_VERSION}-rc1"
 
 # the full "semantic verersion" including the patch number
-BRICK_FULL_VERSION = f"{BRICK_VERSION}.{BRICK_PATCH_VERSION}"
+BRICK_FULL_VERSION = f"{BRICK_VERSION}.{BRICK_PATCH_VERSION}-rc1"
