efb6ee372522337868fe9eaa3b50312f01486598
Gabe Fierro <gtfierro@mines.edu>
2024-08-07 16:35:29 -0600

    formatting on test

--- tests/test_relationships.py
+++ tests/test_relationships.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append("..")
 from bricksrc.namespaces import BRICK, RDF, RDFS, OWL, REC  # noqa: E402
 from bricksrc.relationships import relationships  # noqa:E402
@@ -11,9 +12,15 @@ def test_relationship_identification(brick):
     """
     relships = set(brick.subjects(RDFS.subPropertyOf, BRICK.Relationship))
     all_properties = set(brick.subjects(RDF.type, OWL.ObjectProperty))
-    all_properties = all_properties.union(set(brick.subjects(RDF.type, OWL.DatatypeProperty)))
+    all_properties = all_properties.union(
+        set(brick.subjects(RDF.type, OWL.DatatypeProperty))
+    )
     # keep only properties in the Brick and REC namespaces
-    all_properties = {p for p in all_properties if p.startswith(BRICK) or p.startswith(REC)}
+    all_properties = {
+        p for p in all_properties if p.startswith(BRICK) or p.startswith(REC)
+    }
     non_brick_properties = all_properties - relships
 
-    assert len(relships) + len(non_brick_properties) == len(relationships), "There are properties in the built ontology which do not appear in the relationships.py file. Make sure to add all properties to relationships.py."
+    assert (
+        len(relships) + len(non_brick_properties) == len(relationships)
+    ), "There are properties in the built ontology which do not appear in the relationships.py file. Make sure to add all properties to relationships.py."
